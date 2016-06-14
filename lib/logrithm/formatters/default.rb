c96087973d082774d60d4f2834fcbdd96645e25b
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2016-06-14 16:30:00 +0200

    Default log: fixed.

--- lib/logrithm/formatters/default.rb
+++ lib/logrithm/formatters/default.rb
@@ -2,7 +2,7 @@ module Logrithm
   module Formatters
     module Default
       def formatter
-        rails? ? Kernel.const_get('::Rails').logger.formatter : Logger::Formatter.new
+        Logrithm.rails? ? Kernel.const_get('::Rails').logger.formatter : Logger::Formatter.new
       end
       module_function :formatter
     end
